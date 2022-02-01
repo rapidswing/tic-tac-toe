34b4bedeb68f76f611e9add4c588fa2ed749abd2
Bradley Wright <bradley.wright@gmail.com>
2022-02-01 00:00:32 -0500

    Added Outfit font

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import { PAGES } from "utilities/constants";
 import NewGame from 'views/NewGame/NewGame'
 import Game from "views/Game/Game";
 
+import WebFont from 'webfontloader';
+
 import './App.scss';
+import { useEffect } from "react";
 
 function App() {
   const page = useSelector((state) => state.app.page);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Outfit']
+      }
+    })
+  });
 
   return (
     <div className="App">
